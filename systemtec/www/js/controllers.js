88c0906379ead5441be1f73e3f261efbc3aaef40
Israel designer <israel.kotzen@gmail.com>
2016-05-15 14:38:22 -0300

    adicionando trabalhocontroller.js no arquivo controller.js e montando lista de trabalhos

--- systemtec/www/js/controllers.js
+++ systemtec/www/js/controllers.js
@@ -3,7 +3,7 @@
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout, $http) {
     // Form data for the login modal
     $scope.loginData = {};
     $scope.isExpanded = false;
@@ -220,6 +220,64 @@ angular.module('starter.controllers', [])
     ionicMaterialMotion.fadeSlideInRight({
         selector: '.animate-fade-slide-in .item'
     });
+
+
+//////CONTROLLERS LISTAR TRABALHOS//////////////////////
+
+var trabalho = {
+    contador:0,
+    Id: "",
+    Tema: "",
+    Descricao: "",
+    DataApresentacao: "",
+    Ativo:false
+}
+$scope.trabalhos = [];
+$scope.infotrabalho = {
+    contador: 0,
+    Id: "",
+    Tema: "",
+    Descricao: "",
+    DataApresentacao: "",
+    Ativo: false
+}
+
+$scope.listarTrabalhos = function() {
+    $http({
+        url: 'Api/trabalhos/Listar',
+        method: 'GET'
+    }).success(function (data) {
+        $scope.trabalhos.length = 0;
+        angular.forEach(data, function (value, key) {
+            trabalho.contador = key;
+            trabalho.Id = data[key].Id;
+            trabalho.Tema = data[key].Tema;
+            trabalho.Descricao = data[key].Descricao;
+            trabalho.DataApresentacao = data[key].DataApresentacao;
+            trabalho.Ativo = data[key].Ativo;
+
+            $scope.trabalhos.push(angular.copy(trabalho));
+        });
+        return $scope.trabalhos;
+    });
+}
+
+$scope.visualizar = function(codigo) {
+    $http({
+        url: 'Api/trabalhos/info/' + codigo,
+        method: 'GET'
+    }).success(function (data) {
+        angular.forEach(data, function (value, key) {
+            $scope.infotrabalho.contador = key;
+            $scope.infotrabalho.Id = data[key].Id;
+            $scope.infotrabalho.Tema = data[key].Tema;
+            $scope.infotrabalho.Descricao = data[key].Descricao;
+            $scope.infotrabalho.DataApresentacao = data[key].DataApresentacao;
+            $scope.infotrabalho.Ativo = data[key].Ativo;
+        });
+        return $scope.infotrabalho;
+    });
+}
 
 })
 
